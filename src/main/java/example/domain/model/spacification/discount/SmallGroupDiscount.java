e199ce41a0d84818c71f4039e8a09ce91d9aa4d9
setoguchi <setoguchi.takuma@gmail.com>
2019-12-12 10:36:16 +0900

    団体割引の割引率のルールを少人数団体割引クラスに移動

--- src/main/java/example/domain/model/spacification/discount/SmallGroupDiscount.java
+++ src/main/java/example/domain/model/spacification/discount/SmallGroupDiscount.java
@@ -5,6 +5,7 @@ import example.domain.model.spacification.schedule.DepartureDate;
 import example.domain.model.spacification.NumberOfPeople;
 
 import java.math.BigDecimal;
+import java.time.MonthDay;
 
 /**
  * 少人数の団体割引
@@ -23,9 +24,18 @@ public class SmallGroupDiscount {
         if (人数._8人未満() || !人数._30人以下()) {
             return new DiscountRate(BigDecimal.ONE);
         }
+        return 割引率();
+    }
 
-        return 出発日.割引率();
-
+    // TODO NumberOfPeopleを使っていないので、
+    //  DepartureDateに移動したいが、
+    //  移動すると双方向の依存になってしまう
+    //  少人数の割引率はこのクラスにあったほうが良いかも
+    private DiscountRate 割引率() {
+        if (出発日.isAfter(MonthDay.of(12, 21))
+                && 出発日.isBefore(MonthDay.of(1, 10)))
+            return new DiscountRate(new BigDecimal("0.9"));
+        return new DiscountRate(new BigDecimal("0.85"));
     }
 
     public FareAmount 適用する(FareAmount 合計料金) {
--- src/main/java/example/domain/model/spacification/schedule/DepartureDate.java
+++ src/main/java/example/domain/model/spacification/schedule/DepartureDate.java
@@ -1,9 +1,7 @@
 package example.domain.model.spacification.schedule;
 
-import example.domain.model.spacification.discount.DiscountRate;
 import example.domain.model.spacification.season.SeasonType;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.MonthDay;
 
@@ -19,20 +17,28 @@ public class DepartureDate {
     }
 
     public SeasonType 季節区分() {
-        if (value.isAfter(MonthDay.of(12, 25))
-                && value.isBefore(MonthDay.of(1, 10)))
+        if (isAfter(MonthDay.of(12, 25))
+                && isBefore(MonthDay.of(1, 10)))
             return SeasonType.繁忙期;
-        if (value.isAfter(MonthDay.of(1, 16))
-                && value.isBefore(MonthDay.of(1, 30)))
+        if (isAfter(MonthDay.of(1, 16))
+                && isBefore(MonthDay.of(1, 30)))
             return SeasonType.閑散期;
         return SeasonType.通常期;
     }
 
-    public DiscountRate 割引率() {
-        if (value.isAfter(MonthDay.of(12, 21))
-                && value.isBefore(MonthDay.of(1, 10)))
-            return new DiscountRate(new BigDecimal("0.9"));
-        return new DiscountRate(new BigDecimal("0.85"));
+//    public DiscountRate 割引率() {
+//        if (value.isAfter(MonthDay.of(12, 21))
+//                && value.isBefore(MonthDay.of(1, 10)))
+//            return new DiscountRate(new BigDecimal("0.9"));
+//        return new DiscountRate(new BigDecimal("0.85"));
+//    }
+
+    public boolean isAfter(MonthDay 日付) {
+        return value.isAfter(日付);
+    }
+
+    public boolean isBefore(MonthDay 日付) {
+        return value.isBefore(日付);
     }
 
     @Override
